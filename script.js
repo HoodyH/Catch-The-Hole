566104e39b6f4fa894dd2e759e8adedb4dafcae2
Simone <not.simone.work@outlook.com>
2019-10-22 21:56:16 +0200

    Post bugged works only in local visualization ok

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
+//abbestiaDC, license: do what you want
+
+//const api_url = 'https://abbestiadc.pythonanywhere.com/api/posts/'
+const api_url = 'http://78.134.58.32:8000/api/posts/'
+//const api_url = 'http://10.32.10.96:8000/api/posts/'
+
 function createRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url, true);
-    return xhr;
+    var xhr = new XMLHttpRequest()
+    xhr.open(method, url, true)
+    return xhr
 }
 
-const app = document.getElementById('posts')
+async function setPost(){
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+  const title = document.getElementById('title').value
+  const content = document.getElementById('content').value
+  let data = `{"title": "${title}","content": "${content}"}`
 
-app.appendChild(container)
-  
-var url = 'https://abbestiadc.pythonanywhere.com/api/posts/';
+  fetch(api_url, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: data
+    }).then(res=>res.json())
+      .then(res => console.log(res));
+}
 
-var request = createRequest('GET', url);
+async function getFields(){
 
-request.onload = function() {
-  
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(post => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'wrapper fade-up')
-      
-      const h3 = document.createElement('h3')
-      h3.textContent = post.title
+  const app = document.getElementById('posts')
 
-      const blockquote = document.createElement('blockquote')
-      blockquote.textContent = post.content
+  const container = document.createElement('div')
+  container.setAttribute('class', 'container')
 
-      container.appendChild(card)
-      card.appendChild(h3)
-      card.appendChild(blockquote)
-    })
-  }
-};
+  app.appendChild(container)
+  let request = createRequest('GET', api_url)
 
-request.onerror = function() {
-    console.error('Error', request);
-};
+  request.onload = function() {
 
-request.send()
+    // Begin accessing JSON data here
+    let data = JSON.parse(this.response)
+    if (request.status >= 200 && request.status < 400) {
+      data.reverse().forEach(post => {
+        const card = document.createElement('div')
+        card.setAttribute('class', 'wrapper fade-up')
+
+        const h3 = document.createElement('h3')
+        h3.textContent = post.title
+
+        const blockquote = document.createElement('blockquote')
+        blockquote.textContent = post.content
+
+        container.appendChild(card)
+        card.appendChild(h3)
+        card.appendChild(blockquote)
+      })
+    }
+  };
+
+  request.onerror = function() {
+      console.error('Error', request)
+  };
+
+  request.send()
+}
+
+getFields()
